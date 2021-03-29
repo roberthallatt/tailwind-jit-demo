eb2c22e9edcdfdf9eb57f004e1300a818ddd917a
Robert Hallatt <roberth@cps.ca>
2021-03-29 12:43:54 -0400

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/jit': {},
+    autoprefixer: {},
+    //
+    // uncomment this to enable cssnano
+    //
+    //cssnano: {
+    //  preset: ['default', {
+    //    discardComments: {
+    //        removeAll: true,
+    //    },
+    //  }],
+    //},
+  },
+}
