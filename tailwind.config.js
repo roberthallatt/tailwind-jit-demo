36f6a8ae7263ad965a222a88eaffd1b9c63677ed
roberthallatt <roberth@cps.ca>
2022-05-04 10:38:09 -0400

    Update to latest version of Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
-module.exports =
-{
-  darkMode: false,
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
